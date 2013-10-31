ba039c29478c7362505f1967967f96ed3d8d048f
Nina Andersen <nina@vimond.com>
2013-10-31 16:36:02 +0100

    test to deploy to heroku

---
+++ app/controllers/test_controller.rb
@@ -0,0 +1,7 @@
+class TestController < ApplicationController
+
+  def main
+
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 FilmNett::Application.routes.draw do
+
+  get 'test' => 'test#main'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
