7ff455783cc4fe70b2ca1003a2a874f61704d0d7
ovrebo90@gmail.com <ovrebo90@gmail.com>
2013-10-31 16:09:49 +0100

    all basics

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FilmNett::Application.config.secret_key_base = '100f58c31daf6e880cd3c9b166e5e13d2389d7a43083592054ebf6c267598a5c4b798bfa342502f4d100236c16eb13ec6fc43843c57323c6e09a058ec90f2684'
