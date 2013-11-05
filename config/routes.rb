8d6b6249422f2f26531741f788d070593a38f17f
ovrebo90@gmail.com <ovrebo90@gmail.com>
2013-11-05 13:13:05 +0100

    Made master-layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 FilmNett::Application.routes.draw do
 
   get 'test' => 'test#main'
+  match '/signup',  to: 'users#new',            via: 'get'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
