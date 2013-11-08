e488e64a7dd04c67b663d4c15144c4c0b10b7a55
Nina Andersen <nina@vimond.com>
2013-11-08 02:57:02 +0100

    added searching from - and saving to database

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,8 +3,18 @@ class SearchController < ApplicationController
   require 'filmbuff'
 
   def search_movie
-    imdb = FilmBuff::IMDb.new
-    @movie = imdb.find_by_title(params[:query])
+    movie = Movie.find_by title: params[:query]
+    if movie
+      @movie = movie
+      @from_db = true
+    elsif
+      imdb = FilmBuff::IMDb.new
+      movie = imdb.find_by_title(params[:query])
+      @movie = Movie.new
+      @movie.from_filmbuff(movie)
+      @from_db = false
+      @movie.save
+    end
   end
 
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,17 @@
 class Movie < ActiveRecord::Base
+
+  #attr_accessor :title, :tagline, :plot, :runtime, :rating, :votes, :poster_url, :release_date
+
+  def from_filmbuff(fb)
+    self.imdb_id = fb.imdb_id
+    self.title = fb.title
+    self.tagline = fb.tagline
+    self.plot = fb.plot
+    self.runtime = fb.runtime
+    self.rating = fb.rating
+    self.votes = fb.votes
+    self.poster_url = fb.poster_url
+    self.release_date = fb.release_date
+  end
+
 end
