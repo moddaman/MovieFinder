2e7cfa827203f107c0200becbd770d61b776af93
Nina Andersen <nina@vimond.com>
2013-11-20 14:25:41 +0100

    Added possibility to remove movie from collection

--- app/controllers/listed_movies_controller.rb
+++ app/controllers/listed_movies_controller.rb
@@ -13,4 +13,16 @@ class ListedMoviesController < ApplicationController
     end
   end
 
+  def destroy
+    @user = current_user
+    @movie = Movie.find(params[:movie_id])
+    if @user.unfollow_movie!(@movie)
+      flash[:success] = @movie.title + " was removed from your collection"
+    end
+    respond_to do |format|
+      format.html { redirect_to collection_user_path }
+      format.js
+    end
+  end
+
 end
