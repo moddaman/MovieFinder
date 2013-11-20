889fa7cd9adb86a3513bdf2faaaf845187b448f7
ovrebo90@gmail.com <ovrebo90@gmail.com>
2013-11-20 14:12:55 +0100

    feedback: movie to collecition

--- app/controllers/listed_movies_controller.rb
+++ app/controllers/listed_movies_controller.rb
@@ -2,12 +2,11 @@ class ListedMoviesController < ApplicationController
   before_action :signed_in_user
 
   def create
-    puts "we are here"
     @movie = Movie.find(params[:movie_id])
-    puts "here as well: #{@movie.inspect.to_s}"
     @user = current_user
-    puts "and here: #{@user.inspect.to_s}"
-    current_user.follow_movie(@movie)
+    if current_user.follow_movie(@movie)
+      flash[:success] = @movie.title + " was added to your collection"
+    end
     respond_to do |format|
       format.html { redirect_to @user }
       format.js
